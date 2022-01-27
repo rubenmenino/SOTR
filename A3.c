b43487a9ba5ed698d77fc00f7ae2bcabdf62c331
rubenmenino <ruben.menino@outlook.com>
2022-01-27 14:51:54 +0000

    frontend??

--- A3.c
+++ A3.c
@@ -17,7 +17,6 @@
 /* Kernel includes. */
 #include "FreeRTOS.h"
 #include "task.h"
-
 #include <sys/attribs.h>
 #include <stdlib.h>
 #include "../UART/uart.h" 
@@ -34,6 +33,12 @@
 /* Priorities of the demo application tasks (high numb. -> high prio.) */
 #define OUT_PRIORITY	 tskIDLE_PRIORITY 
 
+SemaphoreHandle_t semA;
+SemaphoreHandle_t semB;
+SemaphoreHandle_t semC;
+SemaphoreHandle_t semD;
+SemaphoreHandle_t semE;
+SemaphoreHandle_t semF;
 
 TaskHandle_t taskHandleA = NULL;
 TaskHandle_t taskHandleB = NULL;
@@ -43,14 +48,32 @@ TaskHandle_t taskHandleE = NULL;
 TaskHandle_t taskHandleF = NULL;
 
 
-
 // code for task
 void createTask(void *pvParam){
     int iTaskTicks = 0;
     uint8_t mesg[80];
     TickType_t pxPreviousWakeTime;
+    TaskHandle_t xTaskGetCurrentHandle();
+    int i, j, IMAXCOUNT, JMAXCOUNT;
     
     for(;;) {
+        //TMAN_TaskWaitPeriod(); // add args if needed
+        int l = periodicityTask() / 2; // GET TICKS
+        char nameTask; // print "Task name " and " ticks"
+        printf("onde estou? ");
+        printf("%d\n", &l);
+        printf("%s , %d", nameTask, l);
+        
+        for(i = 0; i < IMAXCOUNT; i++){
+            for(j = 0; j < JMAXCOUNT; j++){
+                //do some computation to consume time
+            }
+        }
+        // other stuff (if needed))
+        
+        
+        
+        //printf(omega);
         vTaskDelayUntil(&pxPreviousWakeTime,ACQ_PERIOD_MS)
     }
     
@@ -92,15 +115,21 @@ int mainSetrLedBlinkA3(int argc, char** argv) {
     printf("Welcome Message1\n\r");
 
     // Create semaphore before starting tasks
-
+    semA = xSemaphoreCreateBinary();
+    semB = xSemaphoreCreateBinary();
+    semC = xSemaphoreCreateBinary();
+    semD = xSemaphoreCreateBinary();
+    semE = xSemaphoreCreateBinary();
+    semF = xSemaphoreCreateBinary();
+    
     /* Create the tasks defined within this file. */
     
     xTaskCreate( createTask, ( const signed char * const ) "A", configMINIMAL_STACK_SIZE, NULL, OUT_PRIORITY, &taskHandleA );
     xTaskCreate( createTask, ( const signed char * const ) "B", configMINIMAL_STACK_SIZE, NULL, OUT_PRIORITY, &taskHandleB );
-    xTaskCreate( createTask, ( const signed char * const ) "C", configMINIMAL_STACK_SIZE, NULL, OUT_PRIORITY, &taskHandleC );
-    xTaskCreate( createTask, ( const signed char * const ) "D", configMINIMAL_STACK_SIZE, NULL, OUT_PRIORITY, &taskHandleD );
-    xTaskCreate( createTask, ( const signed char * const ) "E", configMINIMAL_STACK_SIZE, NULL, OUT_PRIORITY, &taskHandleE );
-    xTaskCreate( createTask, ( const signed char * const ) "F", configMINIMAL_STACK_SIZE, NULL, OUT_PRIORITY, &taskHandleF );
+    //xTaskCreate( createTask, ( const signed char * const ) "C", configMINIMAL_STACK_SIZE, NULL, OUT_PRIORITY, &taskHandleC );
+    //xTaskCreate( createTask, ( const signed char * const ) "D", configMINIMAL_STACK_SIZE, NULL, OUT_PRIORITY, &taskHandleD );
+    //xTaskCreate( createTask, ( const signed char * const ) "E", configMINIMAL_STACK_SIZE, NULL, OUT_PRIORITY, &taskHandleE );
+    //xTaskCreate( createTask, ( const signed char * const ) "F", configMINIMAL_STACK_SIZE, NULL, OUT_PRIORITY, &taskHandleF );
      
     //TMAN_TaskRegisterAtributes();
     
